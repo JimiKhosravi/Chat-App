a1f215d2d114f6c5a5616420c9570d744fc45a79
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2023-02-03 13:45:32 +0100

    Adding Communication features like image and location

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Platform, KeyboardAvoidingView } from "react-native";
+import { View, StyleSheet, Text, Platform, KeyboardAvoidingView } from "react-native";
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat'
 const firebase = require('firebase');
 require('firebase/firestore');
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from '@react-native-community/netinfo';
+import CustomActions from "./CustomActions";
+import MapView from "react-native-maps";
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 export default class Chat extends React.Component {
   constructor() {
@@ -17,6 +20,9 @@ export default class Chat extends React.Component {
         name: "",
         avatar: "",
       },
+      loggedInText: 'Please wait, you are getting logged in',
+      image: null,
+      location: null,
       isConnected: false
     }
 
@@ -71,6 +77,7 @@ export default class Chat extends React.Component {
               _id: user.uid,
               name: name,
             },
+            loggedInText: ''
           });
           this.unsubscribe = this.referenceChatMessages
             .orderBy('createdAt', 'desc')
@@ -106,6 +113,8 @@ export default class Chat extends React.Component {
       text: message.text || '',
       createdAt: message.createdAt,
       user: message.user,
+      image: message.image || null,
+      location: message.location || null
     });
   };
 
@@ -147,6 +156,8 @@ export default class Chat extends React.Component {
           name: data.user.name,
           avatar: data.user.avatar || '',
         },
+        image: data.image || null,
+        location: data.location || null
       });
     });
     this.setState({
@@ -185,23 +196,52 @@ export default class Chat extends React.Component {
     }
   }
 
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
   render() {
     let color = this.props.route.params.color;
 
     return (
-      <View style={[styles.container, { backgroundColor: color }]}>
-        <GiftedChat
-          renderBubble={this.renderBubble.bind(this)}
-          messages={this.state.messages}
-          renderInputToolbar={this.renderInputToolbar.bind(this)}
-          onSend={(messages) => this.onSend(messages)}
-          user={{
-            _id: this.state.uid,
-            avatar: 'https://placeimg.com/140/140/any',
-          }}
-        />
-        {Platform.OS === 'android' ? (<KeyboardAvoidingView behavior="height" />) : null}
-      </View>
+      <ActionSheetProvider>
+        <View style={[styles.container, { backgroundColor: color }]}>
+          <Text>{this.state.loggedInText}</Text>
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            renderInputToolbar={this.renderInputToolbar.bind(this)}
+            onSend={(messages) => this.onSend(messages)}
+            renderActions={this.renderCustomActions.bind(this)}
+            renderCustomView={this.renderCustomView.bind(this)}
+            user={{
+              _id: this.state.uid,
+              avatar: 'https://placeimg.com/140/140/any',
+            }}
+          />
+          {Platform.OS === 'android' ? (
+            <KeyboardAvoidingView behavior="height" />
+          ) : null}
+        </View>
+      </ActionSheetProvider>
     );
   }
 }
